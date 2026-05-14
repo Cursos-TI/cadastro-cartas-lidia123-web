599e756b28b41937eb5aabe6d001066d9d768431
lidia123-web <lidiaheloisa123@gmail.com>
2026-05-14 00:09:43 +0000

    DASAFIO NIVEL MAESTRE FINALIZADO

--- JogoXadrez.c
+++ JogoXadrez.c
@@ -3,28 +3,43 @@
 int main() {
     printf("--- DESAFIO DE XADREZ ---\n\n");
 
-    // Torre
+    // Torre: 5 casas para a Direita
     printf("Movimento da Torre:\n");
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 5; i++) {
         printf("Direita\n");
         
     }
 
-    // Bispo
+
+    // Bispo: 5 casas na Diagonal (Cima, Direita)
     printf("\nMovimento do Bispo:\n");
     int b = 0;
-    while (b < 2) {
+    while (b < 5) {
         printf("Cima, Direita\n");
         b++;
     }
 
-    // Rainha
+    // Rainha: 8 casas para a Esquerda
     printf("\nMovimento da Rainha:\n");
     int r = 0;
     do {
         printf("Esquerda\n");
         r++;
-    } while (r < 2);
+    } while (r < 8);
+
+    // Cavalo: Movimento em L 
+    printf("\nMovimento do Cavalo:\n");
+
+    int movimentoCompleto = 1; // Flag para controlar o movimento em 'L'
+    while (movimentoCompleto--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cima\n"); // Imprime "Cima" duas vezes
+        }
+        printf("Direita\n"); // imprime "Direita" uma vez
+    }
+    
 
     return 0;
 }
